024a7192c3b132741c37d2fbb33bac20d35cb119
Robin Heinz <robin-heinz@web.de>
2025-01-07 21:54:01 +0100

    removed root element and query provider (artifact from testing)

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from './src/components/Layout';
 
-
-// import "prismjs/themes/prism-solarizedlight.css";
 import "./src/styles/prism-theme.css";
 import "prismjs/plugins/line-highlight/prism-line-highlight.css";
 import "./src/global.css";
 
-const queryClient = new QueryClient();
-
-const RootElement = ({ children }) => {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-};
-
-export const wrapRootElement = ({ element }) => {
-  return <RootElement>{element}</RootElement>;
-};
-
-
-// Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
 )
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Layout from './src/components/layout';
+import Layout from './src/components/Layout';
 
-const queryClient = new QueryClient();
-
-const RootElement = ({ children }) => {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-};
-
-export const wrapRootElement = ({ element }) => {
-  return <RootElement>{element}</RootElement>;
-};
-
-
-// Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
 )
